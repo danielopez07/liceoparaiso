16f21e19ef67cee9e5c20a93dace34f550f2dcf9
Daniel Lopez <danielopez07@gmail.com>
2018-07-08 12:18:01 -0600

    Inicio del frontpage, sin eventos

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
  *
- * @package wprig
+ * @package liceoparaiso
  */
 
 get_header();
@@ -14,20 +14,29 @@ get_header();
 * This call runs only once on index and archive pages.
 * At some point, override functionality should be built in similar to the template part below.
 */
-wp_print_styles( array( 'wprig-content', 'wprig-front-page' ) ); // Note: If this was already done it will be skipped.
-
+// wp_print_styles( array( 'liceoparaiso-content', 'liceoparaiso-front-page', 'liceoparaiso-galleria' ) ); // Note: If this was already done it will be skipped.
+wp_print_styles( array( 'liceoparaiso-content', 'liceoparaiso-galleria' ) ); // Note: If this was already done it will be skipped.
 ?>
+
+		<div class="galleria-content">
+		<?php
+			get_galleria( 77 );
+		?>
+		</div>
+
 	<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		// while ( have_posts() ) :
+		// 	the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+		// 	get_template_part( 'template-parts/content', get_post_type() );
 
-		endwhile; // End of the loop.
+		// endwhile; // End of the loop.
 		?>
+
 		<?php the_posts_navigation(); ?>
+
 
 	</main><!-- #primary -->
 
