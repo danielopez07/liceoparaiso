d3395f7365d83bc1e0d423f2da9000f92d649a76
Daniel Lopez <danielopez07@gmail.com>
2018-07-19 00:19:13 -0600

    id seccion contacto, colores guttenberg comentados (daban problemas)

--- verbose/functions.php
+++ verbose/functions.php
@@ -102,44 +102,44 @@ function liceoparaiso_setup() {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/
 	 */
-	add_theme_support( 'editor-color-palette',
-		array(
-			'name'  => 'Dusty orange',
-			'color' => '#ED8F5B',
-		),
-		array(
-			'name'  => 'Dusty red',
-			'color' => '#E36D60',
-		),
-		array(
-			'name'  => 'Dusty wine',
-			'color' => '#9C4368',
-		),
-		array(
-			'name'  => 'Dark sunset',
-			'color' => '#33223B',
-		),
-		array(
-			'name'  => 'Almost black',
-			'color' => '#0A1C28',
-		),
-		array(
-			'name'  => 'Dusty water',
-			'color' => '#41848F',
-		),
-		array(
-			'name'  => 'Dusty sky',
-			'color' => '#72A7A3',
-		),
-		array(
-			'name'  => 'Dusty daylight',
-			'color' => '#97C0B7',
-		),
-		array(
-			'name'  => 'Dusty sun',
-			'color' => '#EEE9D1',
-		)
-	);
+	// add_theme_support( 'editor-color-palette',
+	// 	array(
+	// 		'name'  => 'Dusty orange',
+	// 		'color' => '#ED8F5B',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty red',
+	// 		'color' => '#E36D60',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty wine',
+	// 		'color' => '#9C4368',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dark sunset',
+	// 		'color' => '#33223B',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Almost black',
+	// 		'color' => '#0A1C28',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty water',
+	// 		'color' => '#41848F',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty sky',
+	// 		'color' => '#72A7A3',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty daylight',
+	// 		'color' => '#97C0B7',
+	// 	),
+	// 	array(
+	// 		'name'  => 'Dusty sun',
+	// 		'color' => '#EEE9D1',
+	// 	)
+	// );
 
 	/**
 	 * Optional: Disable custom colors in block color palettes.
